d6683894903c32c6a98de2b6dafb0a79e26b7047
ethan0401 <71141660+ethan0401@users.noreply.github.com>
2020-10-22 03:17:17 +0000

    i did the screenshot and followed the directions

--- index.java
+++ index.java
@@ -3,6 +3,11 @@ void setup() {
 }
 
 void draw() {
+a = 255;
+y = 120;
+x = 420;
+p = 0;
+q = 175;
 // Step 1: Write code that draws the screenshot in the files 
 // with hard-coded values. (Feel free to use colors 
 // instead of grayscale.)
@@ -15,11 +20,16 @@ void draw() {
 // For example, “variable1 = variable1 + 2;”. 
 // Try different expressions and see what happens!
 
-  background(255);
-  stroke(0);
+  background(a);
+  stroke(p);
   fill(175);
-
-
-  
- 
+ r = random(p,a);
+ g = random(p,a);
+ b = random(p,a);
+ fill(r,g,b);
+ ellipse(y,y,y,y);
+ ellipse(y,x,y,y);
+ ellipse(x,y,y,y);
+ ellipse(x,x,y,y);
+ variable3 = variable3 + 2
 }
